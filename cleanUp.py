b2b3889afe09c91cc274a49614bf54ec828bc1e1
yellucs <21259943+yellucs@users.noreply.github.com>
2019-12-29 01:37:44 +1100

    Update cleanUp.py
    Minified one of the variable names.

--- cleanUp.py
+++ cleanUp.py
@@ -5,6 +5,6 @@ c = 'sudo apt-get dist-upgrade -y'
 d = 'sudo apt-get upgrade -y'
 e = 'sudo apt-get autoremove -y'
 f = 'sudo apt-get clean'
-command = b+a+c+a+d+a+e+a+f
+cmd = b+a+c+a+d+a+e+a+f
 
-subprocess.run(command, shell=True, check=True)
+subprocess.run(cmd, shell=True, check=True)
