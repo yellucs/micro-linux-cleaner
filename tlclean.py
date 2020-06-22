5c36166fdd124bd879eb0bd07ef968dbc5e526e6
yellucs <21259943+yellucs@users.noreply.github.com>
2020-06-22 21:15:32 +1000

    Update tlclean.py

--- tlclean.py
+++ tlclean.py
@@ -6,7 +6,7 @@ d = 'sudo apt-get dist-upgrade -y'
 e = 'sudo apt-get upgrade -y'
 f = 'sudo apt-get autoremove -y'
 g = 'sudo apt-get clean'
-cmd = b+a+c+a+d+a+e+a+f+a+g+b
+cmd = b+a+c+a+d+a+e+a+f+a+g+a+b
 
 subprocess.run(cmd, shell=True, check=True)
 #https://github.com/yellucs/tiny-linux-cleaner/
