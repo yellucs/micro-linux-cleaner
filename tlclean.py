21c4ac8194f693976fa506fc7e413b411a1cb719
yellucs <21259943+yellucs@users.noreply.github.com>
2020-06-22 23:31:23 +1000

    Update tlclean.py

--- tlclean.py
+++ tlclean.py
@@ -10,6 +10,6 @@ g = 'dist-upgrade -y'
 h = 'upgrade -y'
 i = 'autoremove -y'
 j = 'clean'
-cmd = a,b,e+d+c+a,f+c+a,b,g+c+a,b,h+c+a,b,i+c+a,b,j+c+a,b,e
+cmd = a+b+e+d+c+a+f+c+a+b+g+c+a+b+h+c+a+b+i+c+a+b+j+c+a+b+e
 
 subprocess.run(cmd, shell=True, check=True)
