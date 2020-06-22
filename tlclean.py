c90d72214bb776dbec8e02a926658a5110410cc3
yellucs <21259943+yellucs@users.noreply.github.com>
2020-06-22 22:10:30 +1000

    Update tlclean.py

--- tlclean.py
+++ tlclean.py
@@ -1,12 +1,13 @@
+#https://github.com/yellucs/tiny-linux-cleaner/
 import subprocess
 a = ' && '
-b = 'sudo apt-get update --fix-missing'
-c = 'sudo dpkg --configure -a'
-d = 'sudo apt-get dist-upgrade -y'
-e = 'sudo apt-get upgrade -y'
-f = 'sudo apt-get autoremove -y'
-g = 'sudo apt-get clean'
-cmd = b+a+c+a+d+a+e+a+f+a+g+a+b
+b = ' --fix-missing'
+c = 'sudo apt-get update'
+d = 'sudo dpkg --configure -a'
+e = 'sudo apt-get dist-upgrade -y'
+f = 'sudo apt-get upgrade -y'
+g = 'sudo apt-get autoremove -y'
+h = 'sudo apt-get clean'
+cmd = c+b+a+d+a+e+a+f+a+g+a+h+a+c
 
 subprocess.run(cmd, shell=True, check=True)
-#https://github.com/yellucs/tiny-linux-cleaner/
