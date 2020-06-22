bc04a0869b9317d9b862798d7a990bd582157eb0
yellucs <21259943+yellucs@users.noreply.github.com>
2020-06-22 16:47:42 +1000

    Added reconfiguring for packages
    Re-configures packages if upgrade was previously interuppted.

--- cleanUp.py
+++ cleanUp.py
@@ -1,10 +1,11 @@
 import subprocess
 a = ' && '
 b = 'sudo apt-get update --fix-missing'
-c = 'sudo apt-get dist-upgrade -y'
-d = 'sudo apt-get upgrade -y'
-e = 'sudo apt-get autoremove -y'
-f = 'sudo apt-get clean'
-cmd = b+a+c+a+d+a+e+a+f
+c = 'sudo dpkg --configure -a'
+d = 'sudo apt-get dist-upgrade -y'
+e = 'sudo apt-get upgrade -y'
+f = 'sudo apt-get autoremove -y'
+g = 'sudo apt-get clean'
+cmd = b+a+c+a+d+a+e+a+f+a+g
 
 subprocess.run(cmd, shell=True, check=True)
