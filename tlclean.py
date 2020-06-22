aeb25cf1c03cd1266f7ca9fe97d6fe888acc9143
yellucs <21259943+yellucs@users.noreply.github.com>
2020-06-22 18:04:41 +1000

    Added a repository update as the last job

--- tlclean.py
+++ tlclean.py
@@ -6,7 +6,7 @@ d = 'sudo apt-get dist-upgrade -y'
 e = 'sudo apt-get upgrade -y'
 f = 'sudo apt-get autoremove -y'
 g = 'sudo apt-get clean'
-cmd = b+a+c+a+d+a+e+a+f+a+g
+cmd = b+a+c+a+d+a+e+a+f+a+g+b
 
 subprocess.run(cmd, shell=True, check=True)
 #https://github.com/yellucs/tiny-linux-cleaner/
