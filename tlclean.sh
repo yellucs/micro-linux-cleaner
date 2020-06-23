d6a131a199a3ce57236f8d941af30e96ac2ee59e
yellucs <21259943+yellucs@users.noreply.github.com>
2020-06-23 15:07:13 +1000

    Bash Version

---
+++ tlclean.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+a='sudo '
+b='apt-get '
+c=' && '
+d=' --fix-missing'
+e='update'
+f='dpkg --configure -a'
+g='dist-upgrade -y'
+h='upgrade -y'
+i='autoremove -y'
+j='clean'
+cmd=$a$b$e$d$c$a$f$c$a$b$g$c$a$b$h$c$a$b$i$c$a$b$j$c$a$b$e
+output=$(eval "$cmd")
+echo "$output"
